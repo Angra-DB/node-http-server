b9f5fc8ae232be852ba2964eb4c584c584702a27
gabriel <gabrielmesquita@cjr.org.br>
2016-10-07 10:43:07 -0300

    Lookup,delete and update operations added.

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ var http = require ('http');
 var dispatcher = require('httpdispatcher');
 var net = require('net');
 var PORT = 3000;
+var url = require('url');
 
 
 
-dispatcher.onGet("/teste", function(req, res) {
+dispatcher.onGet("/save", function(req, res) {
 
     var client = net.createConnection({port: 1234}, function() {
-      console.log("conectou!");
-      client.write("save erlang");
+      console.log("conectou! - Executando operação de save");
+      client.write("save {Erlang}");
     });
 
     var resData;
     client.on('data', function(data) {
-      console.log("chegou data: " + data);
+      console.log("Documento salvo com sucesso , Dados Recebidos: " + data);
       client.destroy();
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end(data);
@@ -22,6 +23,75 @@ dispatcher.onGet("/teste", function(req, res) {
 
     client.on('close', function() {
 	     console.log('Connection closed');
+    });
+});
+
+dispatcher.onGet("/lookup?id=1", function(req, res) {
+
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    var client = net.createConnection({port: 1234}, function() {
+      console.log("conectou! - Executando operação de lookup com id: " + query);
+      client.write("lookup " + query);
+    });
+
+    var resData;
+    client.on('data', function(data) {
+      console.log("Documento recuperado com sucesso , Dados Recebidos: " + data);
+      client.destroy();
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(data);
+    });
+
+    client.on('close', function() {
+       console.log('Connection closed');
+    });
+});
+
+dispatcher.onGet("/delete?id=1", function(req, res) {
+
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    var client = net.createConnection({port: 1234}, function() {
+      console.log("conectou! - Executando operação de delete com id: " + query);
+      client.write("delete " + query);
+    });
+
+    var resData;
+    client.on('data', function(data) {
+      console.log("Documento deletado com sucesso , Dados Recebidos:  " + data);
+      client.destroy();
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(data);
+    });
+
+    client.on('close', function() {
+       console.log('Connection closed');
+    });
+});
+
+dispatcher.onGet("/update", function(req, res) {
+
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    var client = net.createConnection({port: 1234}, function() {
+      console.log("conectou! - Executando operação de update");
+      client.write("update 1 {erlang}");
+    });
+
+    var resData;
+    client.on('data', function(data) {
+      console.log("Documento atualizado com sucesso , Dados Recebidos:  " + data);
+      client.destroy();
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(data);
+    });
+
+    client.on('close', function() {
+       console.log('Connection closed');
     });
 });
 
@@ -40,5 +110,4 @@ var server = http.createServer(handleRequest);
 
 server.listen(PORT, function() {
   console.log("Server listening on: http://localhost: " + PORT);
-
 })
